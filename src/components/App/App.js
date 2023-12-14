efe951c6da0b87da8c1ba54426da9a1640ea64c5
João Iacillo <joaopiacillo@outlook.com.br>
2023-12-14 14:47:29 +0000

    Initial project setup

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import "../../assets/css/reset.css";
+import "../../assets/css/common.css";
+
+const App = () => (
+    <Router>
+        {/* Navbar */}
+        {/* Routes */}
+        {/* Footer */}
+    </Router>
+);
+
+export default App;
